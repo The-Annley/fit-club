d958828a080744ba7301dcd08ee8b2f82b0c941f
The-Annley <theannleyprod@gmail.com>
2024-05-05 21:43:31 +0300

    second commit

--- js/components/slider.js
+++
@@ -1,5 +0,0 @@
-import { Navigation, Pagination } from "swiper/modules";
-const programs = new Swiper(".programs__slider", {
-  modules: [Navigation, Pagination],
-  slidesPerView: "auto",
-});
--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-import "/components/slider.js";
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".programs__button--next",
+    prevEl: ".programs__button--prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
